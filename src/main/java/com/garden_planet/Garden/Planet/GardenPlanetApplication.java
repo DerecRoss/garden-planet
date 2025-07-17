59cbed6d2971e9875884e11f58543177cc3e38b4
DerecRoss <derecdz999@gmail.com>
2025-07-17 13:31:23 -0300

    setup: initialize project

---
+++ src/main/java/com/garden_planet/Garden/Planet/GardenPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.garden_planet.Garden.Planet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garden_planet/Garden/Planet/GardenPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden_planet.Garden.Planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
