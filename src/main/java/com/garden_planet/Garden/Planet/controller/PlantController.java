6fd5bbe27f8de4bb883df877830bdf6775455926
DerecRoss <derecdz999@gmail.com>
2025-07-23 13:45:33 -0300

    bug: plants null

--- src/main/java/com/garden_planet/Garden/Planet/controller/PlantController.java
+++ src/main/java/com/garden_planet/Garden/Planet/controller/PlantController.java
@@ -9,10 +9,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/plants")
@@ -21,7 +18,7 @@ public class PlantController {
     private final PlantService plantService;
 
     @PostMapping("/user/{Userid}")
-    public ResponseEntity<PlantModel> save(@PathVariable Long Userid, PlantDto plantDto) throws BadRequestException {
+    public ResponseEntity<PlantModel> save(@PathVariable Long Userid,@RequestBody PlantDto plantDto) throws BadRequestException {
         PlantModel plant = plantService.save(plantDto, Userid);
         return new ResponseEntity<>(plant, HttpStatus.CREATED);
     }
